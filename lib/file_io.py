8eed2a1648927baaa41798b1d39eeac281560849
=Memo <=chepkemoicynthia4@gmail.com>
2024-03-08 08:56:22 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
 def write_file(file_name, file_content):
-    pass
+    """Write content to a .txt file."""
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to a .txt file."""
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read content from a .txt file."""
+    with open(f'{file_name}.txt', 'r') as f:
+        file_content = f.read()
+    return file_content
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+log_content = read_file(file_name="logfile")
+print(log_content)
